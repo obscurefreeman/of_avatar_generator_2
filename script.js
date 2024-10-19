c306d21e1d6d1b9bf56864584d94067e6834021b
obscurefreeman <1093048022@qq.com>
2024-10-19 19:48:42 +0800

    删除东西

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ var boxPicker = new iro.ColorPicker("#boxPicker", {
     borderWidth: 1,
     borderColor: "#fff",
     layout: [
-      {
-        component: iro.ui.Box,
-      },
-      {
-        component: iro.ui.Slider,
-        options: {
-          id: 'hue-slider',
-          sliderType: 'hue'
-        }
-      }
-    ]
-  });
+    {
+      component: iro.ui.Box },
+  
+    {
+      component: iro.ui.Slider,
+      options: {
+        id: 'hue-slider',
+        sliderType: 'hue' } }] });
+  
